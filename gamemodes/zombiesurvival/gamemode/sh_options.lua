f66bba2845c8a16f76c38007a118f7e42c7b4d99
Merciful <148659244+Merciful57@users.noreply.github.com>
2024-07-29 17:58:08 -0700

    Remove free points for doing nothing.

--- gamemodes/zombiesurvival/gamemode/sh_options.lua
+++ gamemodes/zombiesurvival/gamemode/sh_options.lua
@@ -758,7 +758,7 @@ GM.MaxPoisonDamage = 50
 GM.MaxBleedDamage = 50
 
 -- Give humans this many points when the wave ends.
-GM.EndWavePointsBonus = 5
+GM.EndWavePointsBonus = 0
 
 -- Also give humans this many points when the wave ends, multiplied by (wave - 1)
-GM.EndWavePointsBonusPerWave = 1
+GM.EndWavePointsBonusPerWave = 0
