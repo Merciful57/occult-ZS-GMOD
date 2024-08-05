49bef1a62718245bf79845e4f0f37f8dd2c61286
Merciful <148659244+Merciful57@users.noreply.github.com>
2024-08-05 13:34:50 -0700

    Minelayer add + attack compatibility

--- gamemodes/zombiesurvival/entities/weapons/weapon_zs_minelayer/shared.lua
+++ gamemodes/zombiesurvival/entities/weapons/weapon_zs_minelayer/shared.lua
@@ -18,7 +18,7 @@ SWEP.Primary.Delay = 1
 
 SWEP.Primary.Damage = 26.67
 SWEP.Primary.ClipSize = 1
-SWEP.Primary.Automatic = false
+SWEP.Primary.Automatic = true
 SWEP.Primary.Ammo = "impactmine"
 SWEP.Primary.DefaultClip = 7
 SWEP.NoDismantle = true
