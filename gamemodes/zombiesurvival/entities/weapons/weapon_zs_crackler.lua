dbe4b9187047ca363e89c737baf7e2e4aea91ead
Merciful <148659244+Merciful57@users.noreply.github.com>
2024-08-05 10:52:26 -0700

    Rename crackler upgrade

--- gamemodes/zombiesurvival/entities/weapons/weapon_zs_crackler.lua
+++ gamemodes/zombiesurvival/entities/weapons/weapon_zs_crackler.lua
@@ -46,7 +46,7 @@ SWEP.IronSightsPos = Vector(-3, 3, 2)
 
 GAMEMODE:AttachWeaponModifier(SWEP, WEAPON_MODIFIER_MAX_SPREAD, -0.375, 1)
 GAMEMODE:AttachWeaponModifier(SWEP, WEAPON_MODIFIER_MIN_SPREAD, -0.2, 1)
-GAMEMODE:AddNewRemantleBranch(SWEP, 1, "'Crackler' Combat Rifle", "Loses automatic fire rate but gains a bit of damage and accuracy", function(wept)
+GAMEMODE:AddNewRemantleBranch(SWEP, 1, "'Commando' Assault Rifle", "Loses automatic fire rate but gains a bit of damage and accuracy", function(wept)
 	wept.Primary.Damage = wept.Primary.Damage * 1.2
 	wept.Primary.Delay = wept.Primary.Delay * 2
 	wept.Primary.ClipSize = 15
