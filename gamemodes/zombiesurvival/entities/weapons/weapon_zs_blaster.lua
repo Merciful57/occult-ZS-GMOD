76450314047f5b79b228ecb344984b0412b2f66b
Merciful <148659244+Merciful57@users.noreply.github.com>
2024-08-05 09:13:06 -0700

    Add pro reload to buster

--- gamemodes/zombiesurvival/entities/weapons/weapon_zs_blaster.lua
+++ gamemodes/zombiesurvival/entities/weapons/weapon_zs_blaster.lua
@@ -66,3 +66,14 @@ function SWEP:SendWeaponAnimation()
 		end
 	end)
 end
+
+function SWEP:ProcessReloadEndTime()
+	local reloadspeed = self.ReloadSpeed * self:GetReloadSpeedMultiplier()
+	if ( self:Clip1() < 1 ) then 
+		self:SetReloadFinish(CurTime() + (self.ReloadSpeed * 3))
+	else if ( self:Clip1() > 0 ) then
+		self:SetReloadFinish(CurTime() + (self.ReloadSpeed * 2))
+	end
+end
+end
+
