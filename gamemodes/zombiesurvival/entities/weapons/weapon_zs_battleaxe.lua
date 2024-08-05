ba407c055b079410fdbd782847d945aba442a7d2
Merciful <148659244+Merciful57@users.noreply.github.com>
2024-08-04 20:22:22 -0700

    Add pro reload to battleaxe

--- gamemodes/zombiesurvival/entities/weapons/weapon_zs_battleaxe.lua
+++ gamemodes/zombiesurvival/entities/weapons/weapon_zs_battleaxe.lua
@@ -40,3 +40,13 @@ SWEP.ConeMin = 0.3
 
 GAMEMODE:AttachWeaponModifier(SWEP, WEAPON_MODIFIER_CLIP_SIZE, 1, 1)
 GAMEMODE:AttachWeaponModifier(SWEP, WEAPON_MODIFIER_FIRE_DELAY, -0.0175, 1)
+
+function SWEP:ProcessReloadEndTime()
+	local reloadspeed = self.ReloadSpeed * self:GetReloadSpeedMultiplier()
+	if ( self:Clip1() < 1 ) then 
+		self:SetReloadFinish(CurTime() + (self.ReloadSpeed * 3))
+	else if ( self:Clip1() > 0 ) then
+		self:SetReloadFinish(CurTime() + (self.ReloadSpeed * 2))
+	end
+end
+end 
