02106964a5daeea4b68ec77441670d259841a5df
Merciful <148659244+Merciful57@users.noreply.github.com>
2024-08-05 13:11:55 -0700

    Add pro reload to z9000

--- gamemodes/zombiesurvival/entities/weapons/weapon_zs_z9000.lua
+++ gamemodes/zombiesurvival/entities/weapons/weapon_zs_z9000.lua
@@ -65,3 +65,13 @@ function SWEP.BulletCallback(attacker, tr, dmginfo)
 		util.CreatePulseImpactEffect(tr.HitPos, tr.HitNormal)
 	end
 end
+
+function SWEP:ProcessReloadEndTime()
+	local reloadspeed = self.ReloadSpeed * self:GetReloadSpeedMultiplier()
+	if ( self:Clip1() < 1 ) then 
+		self:SetReloadFinish(CurTime() + (self.ReloadSpeed * 1.5))
+	else if ( self:Clip1() > 0 ) then
+		self:SetReloadFinish(CurTime() + (self.ReloadSpeed * 1))
+	end
+end
+end
