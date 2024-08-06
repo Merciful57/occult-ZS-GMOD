24711270917c4a7de6354f0a07654a6993bbb374
Merciful <148659244+Merciful57@users.noreply.github.com>
2024-08-06 13:45:33 -0700

    change file path of skill loadouts

--- gamemodes/zombiesurvival/gamemode/sh_options.lua
+++ gamemodes/zombiesurvival/gamemode/sh_options.lua
@@ -22,7 +22,7 @@ GM.ZombieEscapeWeaponsSecondary = {
 -- Change this if you plan to alter the cost of items or you severely change how Worth works.
 -- Having separate cart files allows people to have separate loadouts for different servers.
 GM.CartFile = "zscarts.txt"
-GM.SkillLoadoutsFile = "zsskloadouts.txt"
+GM.SkillLoadoutsFile = "occultskillloadouts.txt"
 
 ITEMCAT_GUNS = 1
 ITEMCAT_AMMO = 2
