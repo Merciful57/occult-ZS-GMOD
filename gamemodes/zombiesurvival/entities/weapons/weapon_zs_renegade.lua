6965a31b3371c746d74027956c0290b6ce8de430
Merciful <148659244+Merciful57@users.noreply.github.com>
2024-08-08 20:47:13 -0700

    pro reload to renegade

--- gamemodes/zombiesurvival/entities/weapons/weapon_zs_renegade.lua
+++ gamemodes/zombiesurvival/entities/weapons/weapon_zs_renegade.lua
@@ -143,3 +143,13 @@ if CLIENT then
 		end
 	end
 end
+
+function SWEP:ProcessReloadEndTime()
+	local reloadspeed = self.ReloadSpeed * self:GetReloadSpeedMultiplier()
+	if ( self:Clip1() < 1 ) then 
+		self:SetReloadFinish(CurTime() + (self.ReloadSpeed * 4))
+	else if ( self:Clip1() > 0 ) then
+		self:SetReloadFinish(CurTime() + (self.ReloadSpeed * 2.8))
+	end
+end
+end 
