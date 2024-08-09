e08fb4c49a36c8b15d1d163ec588424d92b08f75
Merciful <148659244+Merciful57@users.noreply.github.com>
2024-08-08 19:47:13 -0700

    add pro reload to quicksilver

--- gamemodes/zombiesurvival/entities/weapons/weapon_zs_quicksilver.lua
+++ gamemodes/zombiesurvival/entities/weapons/weapon_zs_quicksilver.lua
@@ -79,3 +79,13 @@ if CLIENT then
 		end
 	end
 end
+
+function SWEP:ProcessReloadEndTime()
+	local reloadspeed = self.ReloadSpeed * self:GetReloadSpeedMultiplier()
+	if ( self:Clip1() < 1 ) then 
+		self:SetReloadFinish(CurTime() + (self.ReloadSpeed * 5))
+	else if ( self:Clip1() > 0 ) then
+		self:SetReloadFinish(CurTime() + (self.ReloadSpeed * 3.5))
+	end
+end
+end 
