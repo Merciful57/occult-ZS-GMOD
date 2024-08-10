034d1e7c0695dfef13d57fdf0478bbfa3726d21f
Merciful <148659244+Merciful57@users.noreply.github.com>
2024-08-09 17:39:01 -0700

    Add icon for legendary weapon tab

--- gamemodes/zombiesurvival/gamemode/cl_options.lua
+++ gamemodes/zombiesurvival/gamemode/cl_options.lua
@@ -8,8 +8,8 @@ GM.ItemCategoryIcons = {
 	[ITEMCAT_TOOLS] = "icon16/wrench.png",
 	[ITEMCAT_DEPLOYABLES] = "icon16/package.png",
 	[ITEMCAT_OTHER] = "icon16/world.png",
-	[ITEMCAT_TRINKETS] = "icon16/ruby.png" --[[,
-	[ITEMCAT_RETURNS] = "icon16/user_delete.png"]]
+	[ITEMCAT_TRINKETS] = "icon16/ruby.png",
+	[ITEMCAT_LEGENDARY] = "icon16/gun.png"
 }
 
 GM.RemortColors = {
