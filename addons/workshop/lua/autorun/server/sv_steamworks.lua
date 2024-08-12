ae53f94536f46ec7160d2654e5fbceec6e107581
Merciful <148659244+Merciful57@users.noreply.github.com>
2024-08-12 12:48:09 -0700

    add steam workshop

--- addons/workshop/lua/autorun/server/sv_steamworks.lua
+++ addons/workshop/lua/autorun/server/sv_steamworks.lua
@@ -1,1 +1,4 @@
-resource.AddWorkshop("3297720625")
+resource.AddWorkshop("3297718722") --Merciful
+resource.AddWorkshop("2599978582") --CSS
+resource.AddWorkshop("3304883162") --Finna
+resource.AddWorkshop("3305437193") --Chicken
