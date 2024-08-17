8313849bf3529e72079d78109141d4c0ae3bc1fc
Merciful <148659244+Merciful57@users.noreply.github.com>
2024-08-17 06:09:26 -0700

    add jarjar workshop

--- addons/workshop/lua/autorun/server/sv_steamworks.lua
+++ addons/workshop/lua/autorun/server/sv_steamworks.lua
@@ -2,3 +2,4 @@ resource.AddWorkshop("3297718722") --Merciful
 resource.AddWorkshop("2599978582") --CSS
 resource.AddWorkshop("3304883162") --Finna
 resource.AddWorkshop("3305437193") --Chicken
+resource.AddWorkshop("3310883338")  --Jarjar
