09f66f33698920bb71ddafb832c7c431cc8d6215
Merciful <148659244+Merciful57@users.noreply.github.com>
2024-08-21 01:39:54 -0700

    remove self damage from gas grenade

--- gamemodes/zombiesurvival/entities/entities/projectile_corgasgrenade/init.lua
+++ gamemodes/zombiesurvival/entities/entities/projectile_corgasgrenade/init.lua
@@ -49,7 +49,7 @@ function ENT:AcceptInput(name, activator, caller, arg)
 
 	local vPos = self:GetPos()
 	for _, ent in pairs(ents.FindInSphere(vPos, self.Radius)) do
-		if ent and (ent:IsValidLivingPlayer() and (ent:Team() == TEAM_UNDEAD or ent == owner)) and WorldVisible(vPos, ent:NearestPoint(vPos)) then
+		if ent and (ent:IsValidLivingPlayer() and (ent:Team() == TEAM_UNDEAD)) and WorldVisible(vPos, ent:NearestPoint(vPos)) then
 			if owner:IsValidLivingHuman() then
 				ent.Corrosion = CurTime()
 				ent:EmitSound("player/pl_burnpain" .. math.random(1,3) .. ".wav", 65, math.random(60, 70))
