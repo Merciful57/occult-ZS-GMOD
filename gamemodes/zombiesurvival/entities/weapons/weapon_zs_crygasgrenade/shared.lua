45adcefe8601b5819b4ee281e67571f18f8a5fef
Merciful <148659244+Merciful57@users.noreply.github.com>
2024-08-21 16:13:06 -0700

    name standardize cryo grenade

--- gamemodes/zombiesurvival/entities/weapons/weapon_zs_crygasgrenade/shared.lua
+++ gamemodes/zombiesurvival/entities/weapons/weapon_zs_crygasgrenade/shared.lua
@@ -1,4 +1,4 @@
-SWEP.PrintName = "Cryo Gas Grenade"
+SWEP.PrintName = "'Cryo' Gas Grenade"
 SWEP.Description = "Spews cryo gas in an area where it lands for a significant period of time. Zombies in the area take damage over time and move and attack slower."
 
 SWEP.Base = "weapon_zs_basethrown"
