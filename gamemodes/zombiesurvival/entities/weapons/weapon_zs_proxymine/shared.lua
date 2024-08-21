2bb2667a444d4275c5cce002bf845ca917d9729f
Merciful <148659244+Merciful57@users.noreply.github.com>
2024-08-21 16:11:07 -0700

    fix script error

--- gamemodes/zombiesurvival/entities/weapons/weapon_zs_proxymine/shared.lua
+++ gamemodes/zombiesurvival/entities/weapons/weapon_zs_proxymine/shared.lua
@@ -1,4 +1,4 @@
-SWEP.PrintName = ""Bouncing Betty" Proximity Mine"
+SWEP.PrintName = "'Bouncing Betty' Proximity Mine"
 SWEP.Description = "A proximity detection mine that jumps into the air then explodes, dealing explosive damage."
 
 SWEP.Base = "weapon_zs_basethrown"
