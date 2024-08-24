e62700102672571c1f74b2ca015a7b41bf3db323
Merciful <148659244+Merciful57@users.noreply.github.com>
2024-08-23 23:41:05 -0700

    missing apostrophe

--- gamemodes/zombiesurvival/entities/weapons/weapon_zs_hammer/shared.lua
+++ gamemodes/zombiesurvival/entities/weapons/weapon_zs_hammer/shared.lua
@@ -1,6 +1,6 @@
 SWEP.Base = "weapon_zs_basemelee"
 
-SWEP.PrintName = "Carpenter' Hammer"
+SWEP.PrintName = "'Carpenter' Hammer"
 SWEP.Description = "A simple but extremely useful tool. Allows you to hammer in nails to make barricades.\nPress SECONDARY FIRE to hammer in nail. It will be attached to whatever is behind it.\nPress RELOAD to take a nail out.\nUse PRIMARY FIRE to bash zombie brains or to repair damaged nails.\nYou get a point bonus for repairing damaged nails but a point penalty for removing another player's nails."
 
 SWEP.DamageType = DMG_CLUB
