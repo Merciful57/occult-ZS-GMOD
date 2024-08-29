1f2ad3c5a40f4b41e3aa889056dacede9800f1b0
Merciful <148659244+Merciful57@users.noreply.github.com>
2024-08-29 01:07:14 -0700

    standardize frag grenade name

--- gamemodes/zombiesurvival/entities/weapons/weapon_zs_grenade/shared.lua
+++ gamemodes/zombiesurvival/entities/weapons/weapon_zs_grenade/shared.lua
@@ -1,4 +1,4 @@
-SWEP.PrintName = "Grenade"
+SWEP.PrintName = "'Frag' Grenade"
 SWEP.Description = "A simple fragmentation grenade.\nWhen used in the right conditions, it can obliterate groups of zombies.\nDeals explosive damage."
 
 SWEP.Base = "weapon_zs_basethrown"
