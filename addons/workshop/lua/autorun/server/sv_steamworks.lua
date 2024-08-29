b704c1019a52223ff728a912ef114d09a01334c3
Merciful <148659244+Merciful57@users.noreply.github.com>
2024-08-29 03:56:35 -0700

    remove dead CSS addon

--- addons/workshop/lua/autorun/server/sv_steamworks.lua
+++ addons/workshop/lua/autorun/server/sv_steamworks.lua
@@ -1,5 +1,5 @@
 resource.AddWorkshop("3297718722") --Merciful
-resource.AddWorkshop("2599978582") --CSS
+--resource.AddWorkshop("2599978582") --CSS, RIP
 resource.AddWorkshop("3304883162") --Finna
 resource.AddWorkshop("3305437193") --Chicken
 resource.AddWorkshop("3310883338")  --Jarjar
