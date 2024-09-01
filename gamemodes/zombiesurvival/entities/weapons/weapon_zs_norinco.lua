b04aaef5e1df737674d84fb890e09e8a18840720
Merciful <148659244+Merciful57@users.noreply.github.com>
2024-09-01 10:46:49 -0700

    increase norinco damage

--- gamemodes/zombiesurvival/entities/weapons/weapon_zs_norinco.lua
+++ gamemodes/zombiesurvival/entities/weapons/weapon_zs_norinco.lua
@@ -27,7 +27,7 @@ SWEP.UseHands = true
 
 SWEP.ReloadSound = Sound("Weapon_SG552.Clipout")
 SWEP.Primary.Sound = Sound("Weapon_SG552.Single")
-SWEP.Primary.Damage = 10
+SWEP.Primary.Damage = 20
 SWEP.Primary.NumShots = 1
 SWEP.Primary.Delay = 0.33
 
